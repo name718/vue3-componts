d5e916f062f42ff70fd5fe6cbd0aafd1018b7dac
majuntao <1547996098@qq.com>
2024-12-05 22:00:56 +0800

    实现 icon 组件

---
+++ packages/components/icon/index.ts
@@ -0,0 +1,17 @@
+import {withInstall} from "@mjt/utils/with-install";
+import _Icon from './src/icon.vue'
+
+const Icon = withInstall(_Icon)
+
+export default Icon
+
+export * from './src/icon'
+
+// 这段代码的作用是扩展 Vue 的全局组件类型定义，
+// 使得 ZIcon 组件可以在任何地方（无需显式注册）
+// 被自动识别和使用，同时能够提供类型推断支持。
+declare module 'vue' {
+    export interface GlobalComponents {
+        ZIcon: typeof Icon
+    }
+}
